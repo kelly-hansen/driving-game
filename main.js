24846568d6c6ead013e6abc72ca5d6994f0faeeb
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-05 13:08:54 -0800

    Streamline className direction change

--- main.js
+++ main.js
@@ -6,16 +6,13 @@ var $car = document.querySelector('img');
 
 document.addEventListener('keydown', function (e) {
   if (e.code === 'ArrowUp') {
-    $car.className = 'north';
     data.direction = 'north';
   } else if (e.code === 'ArrowRight') {
-    $car.className = 'east';
     data.direction = 'east';
   } else if (e.code === 'ArrowDown') {
-    $car.className = 'south';
     data.direction = 'south';
   } else if (e.code === 'ArrowLeft') {
-    $car.className = 'west';
     data.direction = 'west';
   }
+  $car.className = data.direction;
 });
